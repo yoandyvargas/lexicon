454729ce3662b4937eb876becdc424f850db81a7
Yoandy Vargas <yoandy.vargas@icloud.com>
2021-07-16 01:29:46 -0400

    Adds more styling to main app

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import './App.css';
 import SearchForm from './Components/SearchForm'
 import Results from './Components/Results'
 import SuggestionsBlock from './Components/SuggestionsBlock'
 import { useState } from 'react'
+import styles from './Styles/App.module.scss'
 const Owlbot = require('owlbot-js');
 const client = Owlbot("cc79e2f4add1dac1bdd8949cbfb560bd3bc12ba5");
 
 function App() {
   //results section that displays word, pronunciation, etc
   //keep a history of previously searched words.
+  //create a set of themes and maybe randomly pick one on load and add a theme picker
+  //look into react transition
 
   const [currentWord, setWord] = useState({ "word": "", "pronunciation": "", "definitions": "" })
 
@@ -21,7 +23,8 @@ function App() {
   }
 
   return (
-    <div className="App">
+    <div className={styles.container}>
+      <h1>Lexicon</h1>
       <Results
         word={currentWord.word}
         pronunciation={currentWord.pronunciation}
--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,8 +1,10 @@
+import styles from '../Styles/Results.module.scss'
+
 export default function Results({ word, pronunciation }) {
   //need to add conditionals for image_url and emoji
 
   return (
-    <div>
+    <div className={styles.container}>
       <h3>{word}</h3>
       <p>{pronunciation}</p>
     </div>
