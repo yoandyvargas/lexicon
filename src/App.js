85884c3c45ea4cb9e7da06fc34534a0d073bc52f
Yoandy Vargas <yoandy.vargas@icloud.com>
2021-07-17 01:57:43 -0400

    Added conditionals and fetch a default state versus adding my own

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import SearchForm from './Components/SearchForm'
 import Results from './Components/Results'
 import SuggestionsBlock from './Components/SuggestionsBlock'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import styles from './Styles/App.module.scss'
 import './Styles/Variables.module.scss'
 const Owlbot = require('owlbot-js');
@@ -13,7 +13,7 @@ function App() {
   //create a set of themes and maybe randomly pick one on load and add a theme picker
   //look into react transition
 
-  const [currentWord, setWord] = useState({ "word": "Yoandy", "pronunciation": "(YO-AHN-DEE)", "definitions": "" })
+  const [currentWord, setWord] = useState({})
   const [isLoading, setIsLoading] = useState(true)
 
   const fetchWordData = value => {
@@ -23,13 +23,14 @@ function App() {
       console.log(currentWord)
   }
 
+  useEffect(() => {
+    fetchWordData('lexicon')
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
+
   return (
     <div className={styles.container}>
-      <h1>Lexicon</h1>
-      <Results
-        word={currentWord.word}
-        pronunciation={currentWord.pronunciation}
-      />
+      {isLoading ? <h2>Loading...</h2> : ( <Results word={currentWord.word} pronunciation={currentWord.pronunciation} definitions={currentWord.definitions} /> )}
       <SearchForm
         fetchWordData={fetchWordData}
       />
--- src/Components/Results.js
+++ src/Components/Results.js
@@ -1,11 +1,14 @@
 import styles from '../Styles/Results.module.scss'
 
-export default function Results({ word, pronunciation }) {
+export default function Results({ word, pronunciation, definitions }) {
   //need to add conditionals for image_url and emoji
+
+  console.log(definitions)
 
   return (
     <div className={styles.container}>
       <h3>{word}</h3>
+      <hr></hr>
       <p>{pronunciation}</p>
     </div>
   )
