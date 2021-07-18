78ad878c0decd747ce4020912fb2f8338bbe19e2
Yoandy Vargas <yoandy.vargas@icloud.com>
2021-07-18 18:57:12 -0400

    adds dark/light toggle

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import SearchForm from './Components/SearchForm'
 import Results from './Components/Results'
 import SuggestionsBlock from './Components/SuggestionsBlock'
+import DarkModeToggle from 'react-dark-mode-toggle'
 import { useState, useEffect } from 'react'
 import styles from './Styles/App.module.scss'
-import './Styles/Variables.module.scss'
 const Owlbot = require('owlbot-js');
 const client = Owlbot("cc79e2f4add1dac1bdd8949cbfb560bd3bc12ba5");
 
+
+function ThemeToggler() {
+  const [theme, setTheme] = useState('light')
+  const nextTheme = theme === 'light' ? 'dark' : 'light';
+
+    useEffect(() => {
+      document.body.dataset.theme = theme
+    }, [theme])
+
+  return (
+    <DarkModeToggle
+    className={styles.themeToggle}
+    checked={theme === 'light' ? false : true}
+    onChange={() => setTheme(nextTheme)}
+    />
+  )
+}
+
 function App() {
-  //create a set of themes and maybe randomly pick one on load and add a theme picker
   //look into react transition
   //find or create a list of more unique words for the "fancy" generator. There are 12 words, minimum req 240 variations. Verify they are on owlbot as well.
 
@@ -28,13 +45,14 @@ function App() {
 
   return (
     <div className={styles.container}>
-      {isLoading ? <h2>Loading...</h2> : ( <Results word={currentWord.word} pronunciation={currentWord.pronunciation} definitions={currentWord.definitions} /> )}
       <SearchForm
         fetchWordData={fetchWordData}
       />
+      {isLoading ? <h2>Loading...</h2> : ( <Results word={currentWord.word} pronunciation={currentWord.pronunciation} definitions={currentWord.definitions} /> )}
       <SuggestionsBlock
         fetchWordData={fetchWordData}
       />
+      <ThemeToggler />
     </div>
   );
 }
